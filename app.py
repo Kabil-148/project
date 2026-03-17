7f3356477d679e32f403a47c3c3bb4aa14938b31
Kabil-148 <gmlkabil4g@gmail.com>
2026-03-18 03:41:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello, World! This is kabil This is my first CI/CD Pipeline from scratch. The pipeline is fully Automated! Finally Everything is working fine!
+        self.wfile.write(b"Hello, World! This is kabil This is my first CI/CD Pipeline from scratch. The pipeline is fully Automated! Finally Everything is working fine!Welcome Back ")
         
-                         Welcome Back ")
-
 server = HTTPServer(('0.0.0.0', 5000), MyHandler)
 print("Server running on port 5000...")
 server.serve_forever()
