9ee0ff5a7e64f55e68a34e873f9256367c7e70db
Kabil-148 <gmlkabil4g@gmail.com>
2026-03-18 02:31:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, World! This is my first CI/CD Pipeline from scratch. The pipeline is fully Automated! Finally Everything is working fine!
+        
+                         Welcome Back ")
+
+server = HTTPServer(('0.0.0.0', 5000), MyHandler)
+print("Server running on port 5000...")
+server.serve_forever()
